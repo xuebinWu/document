cedef58171f853ffaeb559c2eaa9c34f8977ac85
ZLZK <>
2019-07-24 19:34:46 +0800

    添加guide文件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,11 +2,24 @@ module.exports = {
   title: "产品部前端开发文档",
   description: "Just do it",
   themeConfig: {
+    displayAllHeaders: true,
     nav: [
       { text: "Home", link: "/" },
       { text: "Guide", link: "/guide/" },
       { text: "ZvUI", link: "http://zvui.lhanyun.com" }
     ],
-    sidebar: ["/", "/page-a", ["/page-b", "Explicit link text"]]
+    sidebar: [
+      {
+        title: "移动端",
+        collapsable: false,
+        children: [["/mobile/guide", "指南"]]
+      },
+      {
+        title: "PC端",
+        children: [
+          /* ... */
+        ]
+      }
+    ]
   }
 };
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ---
 home: true
 heroImage: /hero.png
-actionText: 开始 →
+actionText: 快速开始 →
 actionLink: /guide/
 features:
-- title: 愿景
-  details: 帮你快速开始一个项目，你和你的成员只需要专注于业务逻辑，并且高频率的复用UI库中的组件.
-- title: 技术栈
-  details: Vue全家桶、 element-ui、 vant、 SuperWebviewSDK
+  - title: 愿景
+    details: 帮你快速开始一个项目，你和你的成员只需要专注于业务逻辑，并且高频率的复用UI库中的组件.
+  - title: 技术栈
+    details: Vue全家桶、 element-ui、 vant、 SuperWebviewSDK
 footer: Copyright © 2019-Liu Yu lin
 ---
